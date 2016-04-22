ce60c1609a517a17d44054e3ddbfebace043f9cc
John Hill <john@proofpositiveimaging.com>
2016-04-22 18:38:15 -0400

    new user works, rejects if user exists

--- browser/js/newUser/newUser.js
+++ browser/js/newUser/newUser.js
@@ -1,35 +1,64 @@
 app.config(function ($stateProvider) {
 
-    $stateProvider.state('newUser', {
-        url: '/newUser',
-        templateUrl: 'js/newUser/newUser.html',
-        controller: 'newUserCtrl'
+  $stateProvider.state('newUser', {
+      url: '/newUser',
+      templateUrl: 'js/newUser/newUser.html',
+      controller: 'newUserCtrl',
     });
 
 });
 
-app.factory('newUserFactory', function($http){
-  return  {
-    createNewUser: function(newUser){
-      return  $http.post('/api/members/', newUser)
-        .then(function(user){
+app.factory('newUserFactory', function ($http) {
+  return {
+    createNewUser: function (newUser) {
+      return $http.post('/api/members/', newUser)
+        .then(function (user) {
           return user;
         });
-    }
-  };//end return
-});//end factory
+    },
+
+    checkUser: function (user) {
+      return $http.post('/api/members/confirm',  user)
+        .then(function (back) {
+          if (back.data.message === true) {
+            return true;
+          }else {
+            return false;
+          }
+        });
+    },
+  };  //end return
+}); //end factory
 
 app.controller('newUserCtrl', function ($scope, AuthService, $state, newUserFactory) {
 
-    $scope.newUser = {};
-    $scope.error = null;
+  $scope.newUser = {};
+  $scope.error = null;
+  $scope.userExists = false;
 
-    $scope.sendCreateUser = function () {
-        newUserFactory.createNewUser($scope.newUser)
-          .then(function (user) {
-            $state.go('home');
+  function sendLogin(loginInfo) {
+    AuthService.login(loginInfo).then(function () {
+          $state.go('home');
         }).catch(function () {
-            $scope.error = 'Create New User Failed';
+          $scope.error = 'Invalid login credentials.';
         });
-    };
+  }// end sendLogin
+
+  $scope.sendCreateUser = function () {
+    newUserFactory.checkUser($scope.newUser)
+        .then(function (res) {
+          if (res === true) {
+            $scope.userExists = true;
+            return;
+          } else {
+            newUserFactory.createNewUser($scope.newUser)
+              .then(function (user) {
+                sendLogin($scope.newUser);
+              }).catch(function () {
+                $scope.error = 'Create New User Failed';
+              });
+          }
+        }); //end then
+  };  //end sendCreateUser
+
 });
