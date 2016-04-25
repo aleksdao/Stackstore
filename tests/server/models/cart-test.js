8e6fa808a156565057197ede66064df43c6927db
Dan Metz <daniel.metz@gmail.com>
2016-04-24 23:40:47 -0400

    finished cart model

---
+++ tests/server/models/cart-test.js
@@ -0,0 +1,27 @@
+var dbURI = 'mongodb://localhost:27017/testingDB';
+var clearDB = require('mocha-mongoose')(dbURI);
+
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+
+require('../../../server/db/models');
+
+var Cart = mongoose.model('Cart');
+
+describe('Cart model', function () {
+
+  beforeEach('Establish DB connection', function (done) {
+      if (mongoose.connection.db) return done();
+      mongoose.connect(dbURI, done);
+  });
+
+  afterEach('Clear test database', function (done) {
+      clearDB(done);
+  });
+
+  it('Should Exist', function () {
+    expect(Cart).to.be.a('function');
+  });
+
+});
