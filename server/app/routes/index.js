25bf3117c3c1482090994a678926fa93b839d0ee
Alex Dao <rahx1t@gmail.com>
2016-04-26 01:40:32 -0400

    added checkbox-type filters on all experiences page

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,8 +3,9 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
-router.use('/experiences', require('./experiences'))
+router.use('/experiences', require('./experiences'));
 router.use('/cart', require('./cart'));
+router.use('/categories', require('./categories'));
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
