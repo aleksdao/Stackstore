f09e4882b495b6f2ab7fdc9a47e3fde0df9fd2f7
John Hill <john@proofpositiveimaging.com>
2016-05-02 16:15:18 -0400

    ratings average works

--- seed.js
+++ seed.js
@@ -154,7 +154,8 @@ var seedExperiences = function (addresses, categories, randomizerIdx) {
       experience.category = categories[randomizerIdx(0, categories.length - 1)];
       experience.address = addresses[randomizerIdx(0, addresses.length - 1)];
       experience.photoUrl = _photoUrls[randomizerIdx(0, _photoUrls.length-1)];
-      experience.ratingAverage = randomizerIdx(2,5);
+      experience.averageRating = 0;
+      // experience.ratingAverage = randomizerIdx(2,5);
       experiences.push(experience);
     });
 
