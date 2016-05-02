b35ea68560659879923943cdc865267fbb75c0ed
Alex Dao <rahx1t@gmail.com>
2016-05-02 16:31:16 -0400

    if no env vars passed for nodemailer, skip email notifications

--- server/app/routes/checkout/index.js
+++ server/app/routes/checkout/index.js
@@ -46,23 +46,28 @@ router.post('/', function (req, res, next) {
   .then(function(charge) {
     console.log('charged card', charge);
     orderConfirmation.stripeCharge = charge;
-    var mailOptions = {
-      from: 'rahx1t@gmail.com',
-      to: 'rahx1t@gmail.com',
-      subject: 'Order Confirmation from Experience! Adventures! ' + orderConfirmation.orderId,
-      text: 'Success!'
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      else {
-        orderConfirmation.email = info.response;
-        console.log('Message sent' + info.response);
-        res.send(orderConfirmation);
-      }
-    })
+    if (process.env.NODEMAILER_USER) {
+      var mailOptions = {
+        from: 'rahx1t@gmail.com',
+        to: 'rahx1t@gmail.com',
+        subject: 'Order Confirmation from Experience! Adventures! ' + orderConfirmation.orderId,
+        text: 'Success!'
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          res.send(error);
+        }
+        else {
+          orderConfirmation.email = info.response;
+          console.log('Message sent' + info.response);
+          res.send(orderConfirmation);
+        }
+      })
+    }
+    else {
+      res.send(charge);
+    }
 
   });
 
