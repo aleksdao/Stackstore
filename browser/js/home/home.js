fe88e4a5a0c85d8b60429278d4f85bd32a92c95d
mydbol <kobrien@mydbol.com>
2016-05-07 19:36:21 -0400

    home page update

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,7 +2,35 @@ app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
         controller : function($scope,$state){
- 		$state.go('experiences');
-    	}
-	});
+          $scope.video = {
+            id: 'eZp2CQgxs9c'
+          };
+          $scope.resources = [
+            // 'http://techslides.com/demos/sample-videos/small.webm',
+            '*.ogv',
+            '/video/Experience_Splash.mp4',
+        ];
+        $scope.poster = 'http://placehold.it/2000&text=you%20may%20want%20to%20have%20a%20poster';
+        $scope.fullScreen = false;
+        $scope.muted = true;
+        $scope.zIndex = '99';
+        $scope.playInfo = {};
+        $scope.pausePlay = true;
+        (function() {
+            var i=0;
+            var actionsarray=['SURF','SURF','RIDE','RIDE','RIDE','RIDE','SKI','RUN','RUN','CLIMB','DARE','DREAM','DREAM','LIVE','LIVE'];
+                setInterval(function(){
+                    if(i >= actionsarray.length){return};
+                    console.log('ssss');
+                    document.getElementById('actions').innerHTML='<md-button class="gobutton"><h1 class="splashaction">'+actionsarray[i]+'</h1></md-button>';
+                    if(i == actionsarray.length-1){document.getElementById('actions').innerHTML='<div class="gobuttonlast"><h1 class="splashaction">'+actionsarray[i]+'</h1></div>'};
+                    i++
+                },700);
+            
+            
+            
+        })();
+        }
+     });
+
 });
